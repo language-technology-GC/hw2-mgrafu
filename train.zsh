199d813940fa50ec4aa96e0471d88e0823b8e5e2
Mariana Graterol Fuenmayor <mgrafu@gmail.com>
2021-10-19 22:34:03 -0400

    train model

---
+++ train.zsh
@@ -0,0 +1,19 @@
+fairseq-train \
+    data-bin \
+    --source-lang ice.g \
+    --target-lang ice.p \
+    --seed 42 \
+    --arch lstm \
+    --encoder-bidirectional \
+    --dropout .2 \
+    --encoder-embed-dim 128 \
+    --decoder-embed-dim	128 \
+    --decoder-out-embed-dim	128 \
+    --criterion label_smoothed_cross_entropy \
+    --label-smoothing .1 \
+    --optimizer adam \
+    --lr .001 \
+    --clip-norm 1 \
+    --batch-size 50 \
+    --fp16-scale-window	800 \
+    --no-epoch-checkpoints
