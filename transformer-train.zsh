406e4b85d414d90353522dd4581246c29ee035ea
Mariana Graterol Fuenmayor <mgrafu@gmail.com>
2021-10-23 12:49:58 -0400

    try transformer train w same specs

---
+++ transformer-train.zsh
@@ -0,0 +1,21 @@
+fairseq-train \
+    data-bin \
+    --source-lang ice.g \
+    --target-lang ice.p \
+    --seed 42 \
+    --arch transformer \
+    --encoder-bidirectional \
+    --dropout .2 \
+    --encoder-embed-dim 128 \
+    --decoder-embed-dim 128 \
+    --decoder-out-embed-dim 128 \
+    --decoder-hidden-size=512 \
+    --encoder-hidden-size=512 \
+    --criterion label_smoothed_cross_entropy \
+    --label-smoothing .1 \
+    --optimizer adam \
+    --lr .001 \
+    --clip-norm 1 \
+    --batch-size 50 \
+    --max-update 800 \
+    --no-epoch-checkpoints
